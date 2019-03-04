26905cefa52fd08c797faaaebabf58e954268d86
Susan Kim <suzenyada@gmail.com>
2019-03-04 14:35:46 +0900

    Application setup

---
+++ SimpleBulletinBoard/PostCell.swift
@@ -0,0 +1,27 @@
+//
+//  PostCell.swift
+//  SimpleBulletinBoard
+//
+//  Created by Susan Kim on 04/03/2019.
+//  Copyright © 2019 Susan Kim. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var contentTextView: UITextView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
