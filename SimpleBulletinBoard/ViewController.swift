9f977af0eef4cdf74dd69dc04e3054b290e6866c
Susan Kim <suzenyada@gmail.com>
2019-03-04 14:21:20 +0900

    Initial Commit

---
+++ SimpleBulletinBoard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleBulletinBoard
+//
+//  Created by Susan Kim on 04/03/2019.
+//  Copyright © 2019 Susan Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
