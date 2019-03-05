c184f3a21a3c0fe470d9bb305bd7684670909e65
Susan Kim <suzenyada@gmail.com>
2019-03-05 15:00:02 +0900

    Added Detail view controller

---
+++ SimpleBulletinBoard/DetailVC.swift
@@ -0,0 +1,34 @@
+//
+//  DetailVC.swift
+//  SimpleBulletinBoard
+//
+//  Created by Susan Kim on 05/03/2019.
+//  Copyright © 2019 Susan Kim. All rights reserved.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+
+    var index:String?
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var contentLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
