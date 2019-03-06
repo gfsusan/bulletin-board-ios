9c4dcaeb9d3f63387ad4cfc369bdf9e0c038900d
Susan Kim <suzenyada@gmail.com>
2019-03-06 09:57:10 +0900

    Added Modify view

--- SimpleBulletinBoard/ModifyVC.swift
+++ SimpleBulletinBoard/ModifyVC.swift
@@ -1,52 +1,29 @@
 //
-//  AddVC.swift
+//  ModifyVC.swift
 //  SimpleBulletinBoard
 //
-//  Created by Susan Kim on 05/03/2019.
+//  Created by Susan Kim on 06/03/2019.
 //  Copyright © 2019 Susan Kim. All rights reserved.
 //
 
 import UIKit
 
-class AddVC: UIViewController {
+class ModifyVC: UIViewController {
 
     @IBOutlet weak var titleField: UITextField!
     @IBOutlet weak var contentField: UITextView!
-    
-    var detailVCDelegate: DetailVC?
-    var post: Post?
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        
-        if let currentPost = post {
-            print("dlkfajlkdsfjk")
-            titleField.text = currentPost.title
-            contentField.text = currentPost.content
-        }
     }
     
-
     @IBAction func savePressed(_ sender: Any) {
-        let title = titleField.text!
-        let content = contentField.text!
-        
-        if let currentPost = post {
-            dataCenter.modifyPost(id: currentPost.number, title: title, content: content) {
-                self.dismiss(animated: true, completion: nil)
-            }
-        } else {
-            dataCenter.uploadPost(number: "1", title: title, content: content) {
-                self.dismiss(animated: true, completion: nil)
-            }
-        }
     }
     
     @IBAction func cancelPressed(_ sender: Any) {
-        self.dismiss(animated: true, completion: nil)
     }
+    
     /*
     // MARK: - Navigation
 
